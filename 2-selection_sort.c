c78e310799cbf16ba1f33daab9e4cba93415b70a
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-18 13:51:18 -0800

    sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,14 +20,14 @@ void swaps(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, min;
+	size_t i, j, min;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (arr[j] < arr[min])
+			if (array[j] < array[min])
 			{
 				min = j;
 			}
