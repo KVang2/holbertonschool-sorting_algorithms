7d3aaa0671101bfcf2ef25e8bd6537280299b071
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-18 18:29:58 -0800

    3-sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -18,11 +18,11 @@ void swaps(int *a, int *b)
 */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size > 0)
 	{
 		return;
-	}
 		quick_sort_recursive(array, 0, size - 1);
+	}
 }
 
 /**
